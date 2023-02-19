f3c949ed40f554c87ada8532d88273fa4bca6395
Gabriel Kabbe <gabriel.kabbe@mail.de>
2023-02-19 22:56:16 +0100

    Document status codes

--- api.py
+++ api.py
@@ -1,7 +1,11 @@
+import logging
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from fastapi.openapi.utils import get_openapi
 from fastapi.routing import APIRoute
+
+logger = logging.getLogger(__name__)
 
 
 class User(BaseModel):
@@ -19,12 +23,14 @@ class UserDb:
         self.db = {}
 
     def add(self, user):
+        logger.info("Add user %s", user)
         id = self.max_id
         self.db[id] = user
         self.max_id += 1
         return id
 
     def remove(self, id: int) -> None:
+        logger.info("remove user %s", id)
         del self.db[id]
 
     def get_users(self) -> list[User]:
@@ -44,7 +50,7 @@ def get_users() -> list[User]:
     return user_db.get_users()
 
 
-@app.get("/user/{id}")
+@app.get("/user/{id}", responses={404: {"model": None}})
 def get_user(id: int) -> User:
     try:
         return user_db.get_user(id)
@@ -76,8 +82,8 @@ def create_user(user: User) -> UserResponse:
     return UserResponse(id=id)
 
 
-@app.delete("/user/delete/{id}")
-def delete_user(id: int):
+@app.delete("/user/delete/{id}", responses={404: {"model": None}})
+def delete_user(id: int) -> None:
     try:
         user_db.remove(id)
     except KeyError:
